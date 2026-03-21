16eb4131c467852918db3131a7f0d5ac790c5a5e
Josh Montoya <josh@montoya.io>
2026-03-20 20:19:51 -0700

    clean up BST examples

--- bst_test.go
+++ bst_test.go
@@ -9,10 +9,6 @@ var exampleBST BST[testEntry]
 
 func ExampleBST() {
 	exampleBST = make(BST[testEntry], 0, 1024)
-
-	fmt.Printf("exampleBST: %v\n", exampleBST)
-	// Output:
-	// exampleBST: []
 }
 
 func ExampleBST_Insert() {
@@ -20,16 +16,16 @@ func ExampleBST_Insert() {
 	exampleBST.Insert(testEntry{key: "b", value: "bravo"})
 	exampleBST.Insert(testEntry{key: "c", value: "charlie"})
 	exampleBST.Insert(testEntry{key: "d", value: "delta"})
-
 	fmt.Printf("exampleBST: %v\n", exampleBST)
+
 	// Output:
 	// exampleBST: [{a alpha} {b bravo} {c charlie} {d delta}]
 }
 
 func ExampleBST_Get() {
 	val, ok := exampleBST.Get("a")
-
 	fmt.Printf("exampleBST.Get(%q): %v / %v\n", "a", val, ok)
+
 	// Output:
 	// exampleBST.Get("a"): {a alpha} / true
 }
@@ -50,18 +46,13 @@ func ExampleBST_ForEach() {
 }
 
 func ExampleBST_Cursor() {
-	cursor := exampleBST.Cursor()
-
-	val, ok := cursor.Seek("b")
-	fmt.Printf("cursor.Seek(%q): %v / %v\n", "b", val, ok)
-	// Output:
-	// cursor.Seek("b"): {b bravo} / true
+	exampleCursor = exampleBST.Cursor()
 }
 
 func ExampleBST_Remove() {
 	exampleBST.Remove("b")
-
 	fmt.Printf("exampleBS.Remove(): %v\n", exampleBST)
+
 	// Output:
 	// exampleBS.Remove(): [{a alpha} {c charlie} {d delta}]
 }
